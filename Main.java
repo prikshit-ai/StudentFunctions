3668f7c7b9973667046d3ae5de4fd0fc832707ef
prikshitgaur772@gmail.com <prikshitgaur772@gmail.com>
2025-03-03 23:28:43 +0530

    MainSwitch

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StudentManager manager = new StudentManager();
+        Studentmanager manager = new Studentmanager();
         int choice;
 
         while (true) {
@@ -25,4 +25,37 @@ public class Main {
                 System.out.print("Invalid input! Enter a valid option (1-8): ");
                 sc.next();
             }
-            
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    manager.addStudent(sc);
+                    break;
+                case 2:
+                    manager.displayStudents();
+                    break;
+                case 3:
+                    manager.searchByPrn(sc);
+                    break;
+                case 4:
+                    manager.searchByName(sc);
+                    break;
+                case 5:
+                    manager.searchByPosition(sc);
+                    break;
+                case 6:
+                    manager.updateStudent(sc);
+                    break;
+                case 7:
+                    manager.deleteStudent(sc);
+                    break;
+                case 8:
+                    System.out.println("Exiting program...");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice! Try again.");
+            }
+        }
+    }
+}
