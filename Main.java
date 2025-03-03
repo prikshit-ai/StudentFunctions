fe03a59f39450e253495f78f079946be0958b7b3
prikshitgaur772@gmail.com <prikshitgaur772@gmail.com>
2025-03-03 23:35:26 +0530

    FinalMain

--- Main.java
+++ Main.java
@@ -13,7 +13,25 @@ public class Main {
         Studentmanager manager = new Studentmanager();
         int choice;
 
-        e = sc.nextInt();
+        while (true) {
+            // Display menu options
+            System.out.println("\nStudent Management System");
+            System.out.println("1. Add Student");
+            System.out.println("2. Display All Students");
+            System.out.println("3. Search by PRN");
+            System.out.println("4. Search by Name");
+            System.out.println("5. Search by Position");
+            System.out.println("6. Update Student Details");
+            System.out.println("7. Delete Student");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
+
+            // Ensure valid integer input for menu
+            while (!sc.hasNextInt()) {
+                System.out.print("Invalid input! Enter a valid option (1-8): ");
+                sc.next();
+            }
+            choice = sc.nextInt();
 
             switch (choice) {
                 case 1:
